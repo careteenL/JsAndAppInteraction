79ac0937ea332167090a76c610c1b57771a9a74a
careteenL <15074806497@163.com>
2018-06-28 14:27:02 +0800

    modified readme

--- README.md
+++ README.md
@@ -1,34 +1,51 @@
 # JsAndAppInteraction
 APP内嵌H5页面中JS和APP的交互，可传参，可回调
 
-# 前言
+## 前言
 
 项目的快速迭代过程中，APP中嵌入H5页面已是很常见的做法。
 
 一定会有APP和JS的交互场景，例如JS唤起APP并携带参数...
 
-# 技术栈
+## 交互方式
 
-IOS端使用`WebKit`（Apple官方推荐）
+### 方法一：app端拦截和h5端约定好的特定url
 
-Android端使用`JSBridge`（第三方库）
+```js
+// 不带参
+window.location.href = 'https://jingjiren.focus.cn/backtoapp'
+// 带参
+window.location.href = 'https://jingjiren.focus.cn/backtoapp?data=xxx'
+```
+存在的问题：只解决了js调用原生的问题。至于调用的结果和调用完之后要进行一些页面的回调，通过这个拦截url的方式是没办法进行的。
 
-# 使用
+### 方法二：使用[WebViewJavascriptBridge](https://github.com/marcuswestin/WebViewJavascriptBridge)
 
-## JS端
+本质上，它是通过webview的代理拦截scheme，然后注入相应的JS。
 
-见 https://github.com/careteenL/JsAndAppInteraction/blob/master/ios-index.html
+## 使用WebViewJavascriptBridge
 
-支持带参数、带参数且回调
+### APP端
 
-HTML
+- [ios封装](https://github.com/careteenL/JsAndAppInteraction/blob/master/BaseWKWebViewController.swift)
+
+- [android封装](https://github.com/careteenL/JsAndAppInteraction/blob/master/android.java)
+
+### H5端
+
+**原理：** H5页面 <--> Native App执行被调用Native代码返回调用结果（H5页面执行被调用JavaScript代码并返回调用结果）
+
+**封装** [bridge.js](https://github.com/careteenL/JsAndAppInteraction/blob/master/bridge.js)。
+
+
+[index.html](https://github.com/careteenL/JsAndAppInteraction/blob/master/ios-index.html)中使用：
 ```html
 <button id="btn">模拟调用登录带参数和回调</button>
 ```
 
-使用方式
+[index.js](https://github.com/careteenL/JsAndAppInteraction/blob/master/ios-index.html)中使用：
 ```js
-require('bridge.js')
+require('/path/to/bridge.js');
 // 需要和客户端同学提前约定好相互调用的方法名及参数及回调，包裹所需要用到的函数
 HFWVBridge.wrapNativeFn(['login']);
 document.getElementById('btn').onclick = function() {
@@ -76,35 +93,21 @@ HFWVBridge.add('hideBtn',function(){
 });
 ```
 
-核心封装代码
+## 优点及缺点
 
-- ios: https://github.com/careteenL/JsAndAppInteraction/blob/master/bridge.js
-
-- android: https://github.com/careteenL/JsAndAppInteraction/blob/master/android-demo.html
-
-## IOS端
-
-见 https://github.com/careteenL/JsAndAppInteraction/blob/master/BaseWKWebViewController.swift
-
-## Android端
-
-见 https://github.com/careteenL/JsAndAppInteraction/blob/master/android.java
-
-# 优点及缺点
-
-## 优点
+### 优点
 
 - 移动端不需要再拦截跳转链接，硬编码减少。
 - 支持双向回调，支持异步回调。
 - 安全性高
 
-## 缺点
+### 缺点
 
 - 只支持回调，不支持return
 - JS、IOS、Android三端代码初始化较多，也比较复杂。需要一个全栈大佬，出现问题能及时修复。
 
-# 总结
+## 引用
 
-天才从不犯错，所谓的“错”都是有意为之，不过是为了开启发现大门。  -- 犯了错别慌
+- [WebViewJavascriptBridge](https://github.com/marcuswestin/WebViewJavascriptBridge)
 
-偷偷de 
+- [WebViewJavascriptBridge的详细使用 -简书](https://www.jianshu.com/p/ba6358b1eec3)
--- bridge.js
+++ bridge.js
@@ -1,131 +1,131 @@
-~ function (w) {
-  var nativeObject = 'NativeApp'; // 原生注入的对象
-  var ts = +new Date(); // 匿名回调基础时间戳
-  var callbacks = {}; // 回调
-  var nativeFns = {}; // 原生方法
+~ function(w) {
+	var nativeObject = 'NativeApp'; // 原生注入的对象
+	var ts = +new Date(); // 匿名回调基础时间戳
+	var callbacks = {}; // 回调
+	var nativeFns = {}; // 原生方法
 
-  // 添加回调
-  function add(name, fn) {
-    if (typeof fn !== 'function') return false;
-    if (!callbacks[name]) {
-      callbacks[name] = fn;
-      return true;
-    } else {
-      return false;
-    }
-  }
+	// 添加回调
+	function add(name, fn) {
+		if (typeof fn !== 'function') return false;
+		if (!callbacks[name]) {
+			callbacks[name] = fn;
+			return true;
+		} else {
+			return false;
+		}
+	}
 
-  // 删除回调
-  function remove(name) {
-    if (callbacks[name]) {
-      delete callbacks[name];
-    }
-  }
+	// 删除回调
+	function remove(name) {
+		if (callbacks[name]) {
+			delete callbacks[name];
+		}
+	}
 
-  // 包裹Native 方法
-  function wrap(name) {
-    if (nativeFns[name]) return;
-    nativeFns[name] = function (obj, cb) {
-      if (typeof cb === 'function') {
-        obj.cb = '__cb__' + (ts++);
-        add(obj.cb, cb);
-      }
-      var jsonString = JSON.stringify(obj);
-      if (w[nativeObject]) {
-        w[nativeObject][name](jsonString);
-      } else if (window.webkit.messageHandlers) {
-          window.webkit.messageHandlers[name].postMessage(jsonString)
-      }
-    }
-  }
+	// 包裹Native 方法
+	function wrap(name) {
+		if (nativeFns[name]) return;
+		nativeFns[name] = function(obj, cb) {
+			if (typeof cb === 'function') {
+				obj.cb = '__cb__' + (ts++);
+				add(obj.cb, cb);
+			}
+			var jsonString = JSON.stringify(obj);
+			if (w[nativeObject]) {
+				w[nativeObject][name](jsonString);
+			} else if (window.webkit.messageHandlers) {
+				window.webkit.messageHandlers[name].postMessage(jsonString)
+			}
+		}
+	}
 
-  // 调用JS方法
-  function runJS(name, jsonString) {
-    try {
-      var json = JSON.parse(jsonString || '{}');
-      if (callbacks[name]) {
-        callbacks[name](json);
-      }
-      // 删除一次性回调
-      if (name.indexOf('__cb__') === 0) {
-        remove(name);
-      }
-    } catch (e) {
-      console.error(e);
-    }
-  }
-  // 调用Native方法
-  function runNative(name, params, cb) {
-      alert('runNative');
-    if (!nativeFns[name]) return false;
-    if (typeof params === 'undefined') {
-      params = {};
-    }
-    if (typeof params === 'function') {
-      cb = params;
-      params = {};
-    }
-    nativeFns[name](params, cb);
-    return true;
-  }
-  // iOS
-  w.HFWVBridge = {
-    setNativeObject: function (no) {
-      nativeObject = no;
-    },
-    wrapNativeFn: function (names) {
-      if (Array.isArray(names)) {
-        names.forEach(function (name) {
-          wrap(name);
-        })
-      } else {
-        wrap(names);
-      }
-    },
-    runJS: runJS,
-    runNative: runNative,
-    add: add,
-    remove: remove,
-    native: {
-        login: function(){
-            NativeApp.login();
-        }
-    }
-  }
-  // Android
-  function connectWebViewJavascriptBridge(callback) {
-      if (window.WebViewJavascriptBridge) {
-          callback(WebViewJavascriptBridge)
-      } else {
-          document.addEventListener(
-              'WebViewJavascriptBridgeReady'
-              , function() {
-                  callback(WebViewJavascriptBridge)
-              },
-              false
-          );
-      }
-  }
+	// 调用JS方法
+	function runJS(name, jsonString) {
+		try {
+			var json = JSON.parse(jsonString || '{}');
+			if (callbacks[name]) {
+				callbacks[name](json);
+			}
+			// 删除一次性回调
+			if (name.indexOf('__cb__') === 0) {
+				remove(name);
+			}
+		} catch (e) {
+			console.error(e);
+		}
+	}
+	// 调用Native方法
+	function runNative(name, params, cb) {
+		alert('runNative');
+		if (!nativeFns[name]) return false;
+		if (typeof params === 'undefined') {
+			params = {};
+		}
+		if (typeof params === 'function') {
+			cb = params;
+			params = {};
+		}
+		nativeFns[name](params, cb);
+		return true;
+	}
+	// iOS
+	w.HFWVBridge = {
+		setNativeObject: function(no) {
+			nativeObject = no;
+		},
+		wrapNativeFn: function(names) {
+			if (Array.isArray(names)) {
+				names.forEach(function(name) {
+					wrap(name);
+				})
+			} else {
+				wrap(names);
+			}
+		},
+		runJS: runJS,
+		runNative: runNative,
+		add: add,
+		remove: remove,
+		native: {
+			login: function() {
+				NativeApp.login();
+			}
+		}
+	}
+	// Android
+	function connectWebViewJavascriptBridge(callback) {
+		if (window.WebViewJavascriptBridge) {
+			callback(WebViewJavascriptBridge)
+		} else {
+			document.addEventListener(
+				'WebViewJavascriptBridgeReady',
+				function() {
+					callback(WebViewJavascriptBridge)
+				},
+				false
+			);
+		}
+	}
 
-  connectWebViewJavascriptBridge(function(bridge) {
-      bridge.init(function(message, responseCallback) {
-          console.log('JS got a message', message);
-          var data = {
-              'Javascript Responds': '测试中文!'
-          };
+	connectWebViewJavascriptBridge(function(bridge) {
+		bridge.init(function(message, responseCallback) {
+			console.log('JS got a message', message);
+			var data = {
+				'Javascript Responds': '测试中文!'
+			};
 
-          if (responseCallback) {
-              console.log('JS responding with', data);
-              responseCallback(data);
-          }
-      });
-
-      bridge.registerHandler("functionInJs", function(data, responseCallback) {
-          document.getElementById("show").innerHTML = ("data from Java: = " + data);
-          if (responseCallback) {
-              var responseData = "Javascript Says Right back aka!";
-              responseCallback(responseData);
-          }
-      });
-  })
+			if (responseCallback) {
+				console.log('JS responding with', data);
+				responseCallback(data);
+			}
+		});
+        // 通过 registerHandler 注册app端所需要调用的方法
+		bridge.registerHandler("functionInJs", function(data, responseCallback) {
+			document.getElementById("show").innerHTML = ("data from Java: = " + data);
+			if (responseCallback) {
+				var responseData = "Javascript Says Right back aka!";
+				responseCallback(responseData);
+			}
+		});
+	})
 }(window);
